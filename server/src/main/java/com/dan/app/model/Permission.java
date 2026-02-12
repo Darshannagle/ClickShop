b4c7bbc21cc257607d47f9b571205ca3dcfa8254
Darshan Nagle <nagledarshan12@gmail.com>
2026-02-13 00:51:54 +0530

    Fix : id Type

--- server/src/main/java/com/dan/app/model/Permission.java
+++ server/src/main/java/com/dan/app/model/Permission.java
@@ -3,6 +3,7 @@ package com.dan.app.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -14,7 +15,7 @@ import lombok.Data;
 @Data
 public class Permission {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
     @Column(unique = true)
     private String name;
